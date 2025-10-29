749a4ea1319f7ea4392b408fb51b217e2b5f48e6
Đặng Hoàng Gia Phú <zafus2103@gmail.com>
2025-10-29 14:20:52 +0700

    feat(ios): init swift project

---
+++ apps/ios-notes/ios-notes/ios_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_notesApp.swift
+//  ios-notes
+//
+//  Created by zafus on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
